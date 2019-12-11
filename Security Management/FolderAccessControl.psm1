d04a2bed371254f9eaeaec037a5ff5d3e575eb44
Aiden Webb <aiden.webb@gmail.com>
2019-12-11 11:59:47 +0000

    Removed invalid -Message parameter from Write-Output

--- Security Management/FolderAccessControl.psm1
+++ Security Management/FolderAccessControl.psm1
@@ -474,8 +474,8 @@ Function Add-GroupAccessToFolder{
         $FolderAcl | Set-Acl -Path $FullFolderPath
     }
 
-    Write-Output -Message "Add-GroupAccessToFolder complete"
+    Write-Output "Add-GroupAccessToFolder complete"
     Write-Verbose -Message "Staff group located in OU: $StaffGroup_OU_DistinguishedName"
     Write-Verbose -Message "ACL groups located in OU: $ACL_OU_DistinguishedName"
-    Write-Output -Message "Please add staff to the $StaffGroupName group as required to grant $PermissionLevel access to $FullFolderPath"
+    Write-Output "Please add staff to the $StaffGroupName group as required to grant $PermissionLevel access to $FullFolderPath"
 }
